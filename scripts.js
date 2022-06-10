3189fc4c20ad8e97a11bf6dfc6c3e9b61a1c7209
Ildar Hasanov <ildar_hasanov@mail.ru>
2022-06-10 09:31:16 +0300

    'добавлен_код_в_scripts.js'

--- scripts.js
+++ scripts.js
@@ -1,3 +1,19 @@
 (function(){
-    //some code
-})();
+
+    let showPopup = function(){
+        $('.popup-container').css('display', 'flex');
+    };
+
+    let closePopup = function(){
+        $('.popup-container').css('display', 'none');
+    };
+
+    let clearForm = function(){
+        $('.popup-container > input[type="text"]').val('');
+    };
+    
+    $('.close').on('click', function(){
+        clearForm();
+        closePopup();
+    });
+});
