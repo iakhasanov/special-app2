bfd20854b1700097176fa37edf5494cedb822905
Ildar Hasanov <ildar_hasanov@mail.ru>
2022-06-10 09:57:47 +0300

    sh: wq: command not found
    This reverts commit 81879c556c5b5de00b3d5d53c966fc7c00425d22.

--- scripts.js
+++ scripts.js
@@ -7,8 +7,13 @@
     let closePopup = function(){
         $('.popup-container').css('display', 'none');
     };
-       
+
+    let clearForm = function(){
+        $('.popup-container > input[type="text"]').val('');
+    };
+    
     $('.close').on('click', function(){
+        clearForm();
         closePopup();
     });
 });
