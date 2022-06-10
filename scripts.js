81879c556c5b5de00b3d5d53c966fc7c00425d22
Ildar Hasanov <ildar_hasanov@mail.ru>
2022-06-10 09:33:15 +0300

    'удалено_действие_clearForm_в_scripts.js'

--- scripts.js
+++ scripts.js
@@ -7,13 +7,8 @@
     let closePopup = function(){
         $('.popup-container').css('display', 'none');
     };
-
-    let clearForm = function(){
-        $('.popup-container > input[type="text"]').val('');
-    };
-    
+       
     $('.close').on('click', function(){
-        clearForm();
         closePopup();
     });
 });
